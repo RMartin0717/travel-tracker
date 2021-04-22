14230e60b651b3a9ffb51f5030832fb02d57ca7b
Riley Martin <riley.willow@gmail.com>
2021-04-22 16:25:15 -0600

    Structure Traveler class and testing suite

---
+++ test/traveler-test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Traveler from "../src/traveler";
+import travelerTestingData from "./traveler-testing-data";
+
+describe("Travler", () => {
+  let
+  beforeEach(() => {
+
+  });
+  it("Should have properties that store traveler data", () => {
+
+  });
+  it("Should have a parameter that takes in the user's trip data and store it in a property", () => {
+
+  });
+  it("Should have a method to calculate the amount spent on trips this year including a travel agent's 10% fee", () => {
+
+  });
+});
---
+++ test/traveler-testing-data.js
@@ -0,0 +1,5 @@
+const travelerTestingData = [
+
+]
+
+export default travelerTestingData;
