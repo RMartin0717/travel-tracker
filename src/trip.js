d1841d8ce0da26ceddeaaa7adb8cedc4e3f23106
Riley Martin <riley.willow@gmail.com>
2021-04-23 11:00:18 -0600

    Write Trip methods to get specific trip destination data and calculate trip cost

--- src/trip.js
+++ src/trip.js
@@ -3,19 +3,32 @@ class Trip {
     this.id = tripData.id;
     this.userID = tripData.userID;
     this.destinationID = tripData.destinationID;
-    this.travlers = tripData.travelers;
+    this.travelers = tripData.travelers;
     this.date = tripData.date;
     this.duration = tripData.duration;
     this.status = tripData.status;
     this.suggestedActivities = tripData.suggestedActivities;
     this.destinationsData = destinationsData;
     this.destination = this.getTripDestination();
+    //may need a way to take in user-given tripData for their trip planning
   }
 
   getTripDestination() {
-    const destinationName = this.destinationsData.find(destination => destination.id === this.destinationID)
-    return destinationName
-    //check destinations for tripData.destinationID
+    const destinationData = this.destinationsData.find(destination => destination.id === this.destinationID);
+    return destinationData
+    //can access all properites of destination from here
+  }
+
+  calcTripCost() {
+    const flightCost = this.destination.estimatedFlightCostPerPerson * this.travelers;
+    const lodgingCost = this.destination.estimatedLodgingCostPerDay * this.travelers * this.duration;
+    const tripCostPreFee = flightCost + lodgingCost;
+    const rawTripCost = tripCostPreFee * 1.1;
+    const roundTripCostToHundredths = () => {
+      return Math.round(100 * rawTripCost) / 100;
+    }
+    const tripCost = roundTripCostToHundredths();
+    return tripCost
   }
 }
 
--- test/trips-testing-data.js
+++ test/trips-testing-data.js
@@ -3,7 +3,7 @@ const tripsTestingData = [
   "id": 1,
   "userID": 44,
   "destinationID": 49,
-  "travelers": 1,
+  "travelers": 2,
   "date": "2019/09/16",
   "duration": 8,
   "status": "approved",
